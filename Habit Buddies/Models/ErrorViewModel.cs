fae4b4de815e4e108c83e68fb42e840b0c0d6ebd
polinizal <polina.staneva@yahoo.com>
2023-03-12 19:53:04 +0200

    Add project files.

---
+++ Habit Buddies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Habit_Buddies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
