81031a26e7f6bba9d015523929b9bc981e7b96c4
polinizal <polina.staneva@yahoo.com>
2023-03-13 21:07:58 +0200

    add classes

---
+++ Habit Buddies/Models/Habit.cs
@@ -0,0 +1,47 @@
+﻿namespace Habit_Buddies.Models
+{
+    public class Habit
+    {
+
+        /*
+         Habit model: The habit model represents a habit that a user is trying to form or maintain. It might include properties like:
+
+HabitId: A unique identifier for the habit.
+
+UserId: The ID of the user who created the habit.
+
+Title: The title of the habit.
+
+Description: A description of the habit.
+
+Goal: The user's goal for the habit (e.g. "Exercise for 30 minutes every day").
+
+Frequency: The frequency at which the habit should be performed (e.g. "Daily" or "Weekly"). => maham go tva
+
+StartDate: The date on which the user started the habit.
+
+ReminderTime: The time at which the user should be reminded to perform the habit.
+
+IsCompleted: A flag indicating whether the user has completed the habit for the current day/week.*/
+        public Habit()
+        {
+            
+        }
+        public int HabitId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Goal { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+
+
+    }
+}
---
+++ Habit Buddies/Models/Notification.cs
@@ -0,0 +1,38 @@
+﻿namespace Habit_Buddies.Models
+{
+    public class Notification
+    {
+        public Notification()
+        {
+            
+        }
+
+        /*Reminder model: The reminder model represents a reminder that a user has set for a habit. It might include properties like:
+
+ReminderId: A unique identifier for the reminder.
+
+HabitId: The ID of the habit for which the reminder is set.
+
+UserId: The ID of the user who created the reminder.
+
+ReminderTime: The time at which the reminder should be sent.
+
+IsEnabled: A flag indicating whether the reminder is currently enabled.*/
+
+        public int NotificationId { get; set; }
+
+        public string Description { get; set; }
+
+        public int HabitId { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime NotificationTime { get; set; }
+
+        public bool IsEnabled { get; set; }
+
+
+
+
+    }
+}
