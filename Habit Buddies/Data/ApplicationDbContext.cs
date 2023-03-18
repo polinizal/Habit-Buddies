544335abd70505eff26456b11868c7510aa6e92e
polinizal <polina.staneva@yahoo.com>
2023-03-18 10:09:29 +0200

    example entity connection

--- Habit Buddies/Data/ApplicationDbContext.cs
+++ Habit Buddies/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Habit_Buddies.Data.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 
 namespace Habit_Buddies.Data
 {
@@ -11,7 +12,20 @@ namespace Habit_Buddies.Data
         {
             
         }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    modelBuilder.Entity<Habit>()
+        //        .HasOne(h => h.Notification)
+        //        .WithOne(c => c.Post)
+        //        .HasForeignKey(c => c.PostId)
+        //        .OnDelete(DeleteBehavior.Cascade);
 
+        //    modelBuilder.Entity<Comment>()
+        //        .HasOne(c => c.Post)
+        //        .WithMany(p => p.Comments)
+        //        .OnDelete(DeleteBehavior.NoAction);
+        //}
         public DbSet<Habit> Habit { get; set; }
         public DbSet<Notification> Notification { get; set; }
     }
