58e0f2ae9dfc89f79cadc5da4efb755f248931c3
ygiy <ifku,kvjm>
2023-03-19 13:22:12 +0200

    add friends controller

---
+++ Habit Buddies/Controllers/FriendsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Habit_Buddies.Data;
+using Habit_Buddies.Data.Entities;
+
+namespace Habit_Buddies.Controllers
+{
+    public class FriendsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FriendsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Friends
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Friend.Include(f => f.FriendshipList);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Friends/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Friend == null)
+            {
+                return NotFound();
+            }
+
+            var friend = await _context.Friend
+                .Include(f => f.FriendshipList)
+                .FirstOrDefaultAsync(m => m.FriendshipListId == id);
+            if (friend == null)
+            {
+                return NotFound();
+            }
+
+            return View(friend);
+        }
+
+        // GET: Friends/Create
+        public IActionResult Create()
+        {
+            ViewData["FriendshipListId"] = new SelectList(_context.Set<FriendshipList>(), "Id", "Id");
+            return View();
+        }
+
+        // POST: Friends/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FriendshipListId,FriendUserId")] Friend friend)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(friend);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FriendshipListId"] = new SelectList(_context.Set<FriendshipList>(), "Id", "Id", friend.FriendshipListId);
+            return View(friend);
+        }
+
+        // GET: Friends/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Friend == null)
+            {
+                return NotFound();
+            }
+
+            var friend = await _context.Friend.FindAsync(id);
+            if (friend == null)
+            {
+                return NotFound();
+            }
+            ViewData["FriendshipListId"] = new SelectList(_context.Set<FriendshipList>(), "Id", "Id", friend.FriendshipListId);
+            return View(friend);
+        }
+
+        // POST: Friends/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FriendshipListId,FriendUserId")] Friend friend)
+        {
+            if (id != friend.FriendshipListId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(friend);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FriendExists(friend.FriendshipListId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FriendshipListId"] = new SelectList(_context.Set<FriendshipList>(), "Id", "Id", friend.FriendshipListId);
+            return View(friend);
+        }
+
+        // GET: Friends/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Friend == null)
+            {
+                return NotFound();
+            }
+
+            var friend = await _context.Friend
+                .Include(f => f.FriendshipList)
+                .FirstOrDefaultAsync(m => m.FriendshipListId == id);
+            if (friend == null)
+            {
+                return NotFound();
+            }
+
+            return View(friend);
+        }
+
+        // POST: Friends/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Friend == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Friend'  is null.");
+            }
+            var friend = await _context.Friend.FindAsync(id);
+            if (friend != null)
+            {
+                _context.Friend.Remove(friend);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FriendExists(int id)
+        {
+          return (_context.Friend?.Any(e => e.FriendshipListId == id)).GetValueOrDefault();
+        }
+    }
+}
