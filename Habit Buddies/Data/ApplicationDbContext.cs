e5489eb488b487851bb47605e28f5bd1a5f305b0
polinizal <polina.staneva@yahoo.com>
2023-03-19 13:08:15 +0200

    add friend, friendship, user entities in order to create user-friend connection adding migration Friend

--- Habit Buddies/Data/ApplicationDbContext.cs
+++ Habit Buddies/Data/ApplicationDbContext.cs
@@ -10,7 +10,7 @@ namespace Habit_Buddies.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-            
+
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -21,14 +21,30 @@ namespace Habit_Buddies.Data
                 .HasForeignKey(n => n.HabitId)
                 .OnDelete(DeleteBehavior.Cascade);
 
+            modelBuilder.Entity<Habit>()
+                .HasOne(h => h.User)
+                .WithMany(n => n.Habits)
+                .HasForeignKey(n => n.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
             modelBuilder.Entity<Notification>()
                 .HasOne(n => n.Habit)
                 .WithMany(h => h.Notifications)
                 .OnDelete(DeleteBehavior.NoAction);
 
 
+            modelBuilder.Entity<Friend>(b =>
+            {
+                b.HasKey(x => new { x.FriendshipListId, x.FriendUserId });
+            });
+
+
         }
         public DbSet<Habit> Habit { get; set; }
         public DbSet<Notification> Notification { get; set; }
+
+        public DbSet<Friend> Friend { get; set; }
+        public DbSet<Notification> Friendship { get; set; }
     }
 }
---
+++ Habit Buddies/Data/Entities/Friend.cs
@@ -0,0 +1,11 @@
+﻿namespace Habit_Buddies.Data.Entities
+{
+    public class Friend
+    {
+        public int FriendshipListId { get; set; }
+        public virtual FriendshipList? FriendshipList { get; set; }
+
+        public int FriendUserId { get; set; }
+        public virtual User? FriendUser { get; set; }
+    }
+}
