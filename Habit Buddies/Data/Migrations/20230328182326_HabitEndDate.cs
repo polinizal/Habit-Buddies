31979a8723bdbfd11e23ce5bb8fac47b48073dda
polinizal <polina.staneva@yahoo.com>
2023-03-28 21:28:00 +0300

    add HabitEndDate migration

---
+++ Habit Buddies/Data/Migrations/20230328182326_HabitEndDate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Habit_Buddies.Data.Migrations
+{
+    public partial class HabitEndDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "Habits",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Habits");
+        }
+    }
+}
