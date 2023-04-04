22bd4e4be443304e9fc6d7bd2f79d7a26604c4aa
polinizal <polina.staneva@yahoo.com>
2023-04-04 21:28:24 +0300

    add migrations

---
+++ Habit Buddies/Data/Migrations/20230403205651_HabitNotificationsRelationship.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Habit_Buddies.Data.Migrations
+{
+    public partial class HabitNotificationsRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_Habits_HabitId",
+                table: "Notifications");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_Habits_HabitId",
+                table: "Notifications",
+                column: "HabitId",
+                principalTable: "Habits",
+                principalColumn: "HabitId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_Habits_HabitId",
+                table: "Notifications");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_Habits_HabitId",
+                table: "Notifications",
+                column: "HabitId",
+                principalTable: "Habits",
+                principalColumn: "HabitId");
+        }
+    }
+}
