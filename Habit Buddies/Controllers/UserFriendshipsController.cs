69f7a0270927c2b5fb46d2e6fc18cf406b14e56e
LevikoSUM <kosta.ilev@gmail.com>
2023-04-04 21:42:48 +0300

    fix userFriendship delete

--- Habit Buddies/Controllers/UserFriendshipsController.cs
+++ Habit Buddies/Controllers/UserFriendshipsController.cs
@@ -207,20 +207,35 @@ namespace Habit_Buddies.Controllers
         }
 
         // POST: UserFriendships/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(string id)
+        //{
+        //    if (_context.UserFriendships == null)
+        //    {
+        //        return Problem("Entity set 'ApplicationDbContext.UserFriendship'  is null.");
+        //    }
+        //    var userFriendship = await _context.UserFriendships.FindAsync(id);
+        //    if (userFriendship != null)
+        //    {
+        //        _context.UserFriendships.Remove(userFriendship);
+        //    }
+
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
+
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
+        public async Task<IActionResult> DeleteConfirmed(string userId, string userFriendId)
         {
-            if (_context.UserFriendships == null)
+            var userFriendship = await _context.UserFriendships.FindAsync(userId, userFriendId);
+            if (userFriendship == null)
             {
-                return Problem("Entity set 'ApplicationDbContext.UserFriendship'  is null.");
+                return NotFound();
             }
-            var userFriendship = await _context.UserFriendships.FindAsync(id);
-            if (userFriendship != null)
-            {
-                _context.UserFriendships.Remove(userFriendship);
-            }
-            
+
+            _context.UserFriendships.Remove(userFriendship);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
