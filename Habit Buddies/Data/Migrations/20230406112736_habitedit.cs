9f49f458837a56aef21394ba1e44f173d975506b
Leviko <kosta.ilev@gmail.com>
2023-04-06 15:21:08 +0300

    add complete button for today in detail

---
+++ Habit Buddies/Data/Migrations/20230406112736_habitedit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Habit_Buddies.Data.Migrations
+{
+    public partial class habitedit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastCompletedDate",
+                table: "Habits",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastCompletedDate",
+                table: "Habits");
+        }
+    }
+}
